c36113b44b8d653081567a1636deb8efe913599f
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2025-01-10 21:58:59 +0530

    create database connection

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(morgan('dev'));
 
 const PORT = 3000;
+
+async function main(){
+    await mongoose.connect('mongodb://localhost:27017/cinnamon-app')
+    console.log("connected to databse👍");
+}
+main().catch((err) => console.log("database connection faild", err));
 
 app.listen(PORT, () => {
     console.log(`Express App Listen On Port : ${PORT}`);
