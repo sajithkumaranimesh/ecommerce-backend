ff1ebe366d8b625cb12198ea62f9952bc37ea25f
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2025-01-10 22:47:09 +0530

    create the password hashed schema method

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
 
 
 const UserSchema = new Schema({
@@ -12,6 +13,24 @@ const UserSchema = new Schema({
         required: [true, "Password is required!"],
     },
 });
+
+
+
+
+UserSchema.pre('save', async function (next) {
+
+    const user = this;
+    if(!user.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+
+    user.password = hash;
+
+    next();
+    
+})
+
 
 
 const User = mongoose.model("User", UserSchema);
