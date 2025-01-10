7cc275aae8439f494f264862e28a65e663e04bf6
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2025-01-10 23:29:33 +0530

    login user and generate token function

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,11 +1,39 @@
 const User = require("../models/User");
+const jwt = require('jsonwebtoken');
+const secretKey = 'nimesh-secret-key';
 
 const registerUser = async (req, res) => {
     try{
         const { username, password} = req.body;
         await new User({username, password}).save();
-        res.status(201).json({ message: 'User Registered Successfully'});
+        res.status(201).json({ message: 'user registered successfully!'});
+
     }catch(err){
-        res.status(500).json({ message: 'User Registration Failed'})
+        res.status(500).json({ error: 'user registration failed!'})
+
     }
 }
+
+
+const loginUser = async (req, res) => {
+    try{
+        const { username, password} = req.body;
+        const user = await User.findOne({username});
+        if(!user){
+            return res.status(401).json({ error: "user not found in this username!" });
+        }
+        const isPasswordValid = await user.comparePassword(password);
+        if(!isPasswordValid){
+            return res.status(401).json({ error: "invalid password!"})
+        }
+        const token = jwt.sign({userId: user._id}, secretKey, { expiresIn: '5d'});
+        res.json({token});
+
+    }catch(err){
+        res.status(500).json({ error: 'authentication failed'});
+        
+    }
+}
+
+
+module.exports = {registerUser, loginUser}
