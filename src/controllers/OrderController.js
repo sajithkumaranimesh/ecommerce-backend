ca905cbe2076f84aafc9fef8ae2ef59d4fdaeac2
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2025-01-23 20:34:00 +0530

    reactor order response

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,11 +1,10 @@
-const stripe = require('stripe')('sk_test_51Qi77VDSOdB84Ar5WYOs92YdN2geHGbtt8DPjl6UuJgPiDvAcJ0yvbaGm9qIWpbZs5sLXTl44XCOwIkBPy6ffxkN00RRw5wZwi');
 const orderService = require('../service/OrderService');
 
 
 const createOrder = async (req, res) => {
     try{
         const { cartItems } = req.body;
-        const result = await orderService.createOrder( cartItems );
+        const result = await orderService.createOrder(cartItems);
 
         res.status(201).json(result);
     }catch(error){
--- src/service/OrderService.js
+++ src/service/OrderService.js
@@ -2,8 +2,20 @@ const Order = require('../models/Order');
 const stripe = require('stripe')('sk_test_51Qi77VDSOdB84Ar5WYOs92YdN2geHGbtt8DPjl6UuJgPiDvAcJ0yvbaGm9qIWpbZs5sLXTl44XCOwIkBPy6ffxkN00RRw5wZwi');
 
 
-const createOrder = async ( lineItems ) => {
+const createOrder = async (cartItems) => {
     try{
+        const lineItems = cartItems.map(item => ({
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                    name: item.name,
+                    images: [item.imageUrl],
+                },
+                unit_amount: item.price * 100,
+            },
+            quantity: item.quantity,
+        }));
+        
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             line_items: lineItems,
@@ -21,7 +33,8 @@ const createOrder = async ( lineItems ) => {
 
         return { success: true, message: "Order successfully saved!", data: session };
     }catch(error){
-        throw new Error("Failed to save Order.");
+        throw new Error(error);
+        // throw new Error("Failed to save Order.");
     }
 }
 
