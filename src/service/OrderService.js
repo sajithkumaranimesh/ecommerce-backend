2bd9abaf4b79f4333d12f5f1d6fc70bbe606f189
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2025-02-06 14:17:00 +0530

    refactor create order function and impliment stoke deduct functionality

--- src/service/OrderService.js
+++ src/service/OrderService.js
@@ -1,9 +1,19 @@
 const Order = require('../models/Order');
 const stripe = require('stripe')('sk_test_51Qi77VDSOdB84Ar5WYOs92YdN2geHGbtt8DPjl6UuJgPiDvAcJ0yvbaGm9qIWpbZs5sLXTl44XCOwIkBPy6ffxkN00RRw5wZwi');
-
+const mongoose = require('mongoose');
+const Product = require('../models/Product')
 
 const createOrder = async (cartItems) => {
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
     try{
+
+        if(!cartItems || cartItems.length === 0){
+            throw new Error("Cart items are required to create an order.");
+        }
+
         const lineItems = cartItems.map(item => ({
             price_data: {
                 currency: 'usd',
@@ -16,7 +26,7 @@ const createOrder = async (cartItems) => {
             quantity: item.quantity,
         }));
         
-        const session = await stripe.checkout.sessions.create({
+        const sessionData = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             line_items: lineItems,
             mode: 'payment',
@@ -24,17 +34,32 @@ const createOrder = async (cartItems) => {
             cancel_url: 'http://localhost:3000/cancel',
         })
 
+        for(const item of cartItems){
+            const product = await Product.findById(item._id).session(session);
+            if(!product || product.stock < item.quantity){
+                throw new Error(`Product ${item._id} is out of stock.`);
+            }
+
+            product.stock -= item.quantity;
+            await product.save({ session });
+        }
+
         const newOrder = new Order({
             lineItems: lineItems,
-            sessionId: session.id,
+            sessionId: sessionData.id,
         })
 
-        await newOrder.save();
+        await newOrder.save({ session });
 
-        return { success: true, message: "Order successfully saved!", data: session };
+        await session.commitTransaction();
+        session.endSession();
+
+        return { success: true, message: "Order successfully created!", data: sessionData };
     }catch(error){
-        throw new Error(error);
-        // throw new Error("Failed to save Order.");
+        await session.abortTransaction();
+        session.endSession();
+
+        throw new Error("Failed to create order. Please try again.");
     }
 }
 
